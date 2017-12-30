44352a6cb942989200057ef7af2f46f34c257272
Didac Esteban Andres <didacesteban@gmail.com>
2017-12-30 20:50:24 +0100

    testing using expect

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,33 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   let result = utils.add(7, 65);
 
-  if(result !== 72){
-    throw new Error(`Expected 72 but got ${result}`);
-  }
+  expect(result).toBe(72).toBeA('number');
 });
 
 it('should square the number entered', () => {
   let result = utils.square(7);
 
-  if(result !== 7*7){
-    throw new Error(`Expected 49 but got ${result}`);
-  }
+  expect(result).toBe(49).toBeA('number');
+  // throw error without library
+  // if(result !== 7*7){
+  //   throw new Error(`Expected 49 but got ${result}`);
+  // }
+});
+
+it('should set first name and last name', () => {
+  let user = {
+    age: 25,
+    location: 'Terrassa'
+  };
+
+  let res = utils.setName(user, 'Didac Esteban');
+
+  expect(res).toInclude({
+    firstName: 'Didac',
+    lastName: 'Esteban'
+  });
 });
